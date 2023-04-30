f3cf1f34cd8e26b5f8f451d96478969a2b9fe39a
rez433 <r3zaee@gmail.com>
2023-04-30 21:15:01 +0000

    added login

---
+++ frontend/src/pages/LoginPage.tsx
@@ -0,0 +1,20 @@
+import Header from '../components/Header';
+import Login from "../components/Login";
+
+const LoginPage = () => {
+
+	return (
+		<div>
+			<Header />
+			<section>
+				<div>
+					<h1>Login </h1>
+					<Login />
+				</div>
+			</section>
+		</div>
+	)
+
+}
+
+export default LoginPage
